7d66f5040b5f1b12d7450727fcccd56bd9b34f88
KaleSmug <karl.sandegren@gmail.com>
2022-12-05 15:14:09 +0100

    generator enqueue nullreference excpetion

--- ARPG/Assets/Scripts/WorldGenerator.cs
+++ ARPG/Assets/Scripts/WorldGenerator.cs
@@ -1,5 +1,5 @@
-using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class WorldGenerator : MonoBehaviour
@@ -7,17 +7,50 @@ public class WorldGenerator : MonoBehaviour
     public DungeonRoom[] rooms;
     public int generatedRoomAmount;
     public int desiredRoomAmount;
-    public DungeonRoom currentRoom;
+    public Queue<GameObject> pivotsProcessing;
+    private DungeonRoom currentRoom;
     void Awake()
     {
-        if (enabled)
-        {
-            Instantiate(rooms[Random.Range(0, rooms.Length)], transform.position, transform.rotation);
-        }
+        SpawnFirstRoom();
         while (generatedRoomAmount < desiredRoomAmount)
         {
-            
+            SpawnRoom();
         }
     
+    }
+
+    void SpawnRoom()
+    {
+        transform.position = pivotsProcessing.Dequeue().transform.position;
+        currentRoom = rooms[Random.Range(0, rooms.Length)];
+        if (generatedRoomAmount + currentRoom.pivotPoints.Length + pivotsProcessing.Count < desiredRoomAmount)
+        {
+            Instantiate(currentRoom, transform.position, transform.rotation);
+            generatedRoomAmount++;
+        }
+
+        for (int i = 0; i < currentRoom.pivotPoints.Length; i++)
+        {
+            pivotsProcessing.Enqueue(currentRoom.pivotPoints[i]);
+        }
+        
+    }
+
+    void SpawnFirstRoom()
+    {
+        
+        while (generatedRoomAmount <1)
+        {
+            currentRoom = rooms[Random.Range(0, rooms.Length)];
+            if (currentRoom.pivotPoints.Length >1)
+            {
+                Instantiate(currentRoom, transform.position, transform.rotation);
+                generatedRoomAmount++;
+            }
+        }
+        for (int i = 0; i < currentRoom.pivotPoints.Length; i++)
+        {
+            pivotsProcessing.Enqueue(currentRoom.pivotPoints[i]);
+        }
     }
 }
