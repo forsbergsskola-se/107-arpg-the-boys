5513e31d895db9cf91fb070c0d5b7a7a07aacdb8
M1crob1ology <luca.belluso@elev.ga.lbs.se>
2022-12-06 03:55:55 +0100

    Pretty good movement with a dash

--- ARPG/Assets/Scripts/PlayerMovement.cs
+++ ARPG/Assets/Scripts/PlayerMovement.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -12,14 +13,20 @@ public class PlayerMovement : MonoBehaviour
     
     private CapsuleCollider _col;
     private Rigidbody _rb;
+    private Animator _playerAnimator;
     private bool _grounded;
     private Vector3 _groundNormal = Vector3.up;
     private float _regularSpeed;
     private Vector3 _endVel;
+    private float _invulnerabilityTimer = 0.0f;
+    private bool _isInvulnerable = false;
     
     [Header("Movement Variables")]
     public float moveSpeed = 5f;
     public float acceleration = 2;
+    public float rollSpeed = 5.0f;
+    public float rotationSpeed = 10;
+    public float rollRotationSpeed = 20;
     
     [Header("Move Limitations")] 
     public float friction = 0.6f;
@@ -30,6 +37,7 @@ public class PlayerMovement : MonoBehaviour
     
     [Header("Other movement variables")]
     public float gravityScale = 9.82f;
+    public float rollInvulnerabilityDuration = 1.0f;
     public bool canMove = true;
 
     void Start()
@@ -41,6 +49,19 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         move = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+
+
+        if (canMove && _grounded)
+        {
+            if (_isInvulnerable)
+            {
+                RollTimer();
+            }
+            else if (Input.GetButtonDown("Dash"))
+            {
+                StartRoll();
+            }
+        }
     }
 
     private void FixedUpdate()
@@ -50,29 +71,68 @@ public class PlayerMovement : MonoBehaviour
 
     private void Movement()
     {
-        _endVel = body.InverseTransformVector(_rb.velocity);
-        
+        _endVel = transform.InverseTransformVector(_rb.velocity);
+
         if (canMove)
         {
-            
             if (_grounded)
             {
-                _endVel = Accelerate(_endVel, moveSpeed, acceleration, _groundNormal);
-                _endVel = Friction(_endVel, moveSpeed, friction, _groundNormal);
+                if (!_isInvulnerable)
+                {
+                    _endVel = Accelerate(_endVel, moveSpeed, acceleration, _groundNormal);
+                    _endVel = Friction(_endVel, moveSpeed, friction, _groundNormal);
+                }
             }
         }
-
+        
         if (!_grounded)
         {
             Gravity();
         }
 
-        _rb.velocity = body.TransformVector(_endVel);
+        RotatePlayer();
+        _rb.velocity = transform.TransformVector(_endVel);
+    }
+
+    void RollTimer()
+    {
+        _invulnerabilityTimer += Time.deltaTime;
+        body.rotation = Quaternion.Lerp(body.rotation, Quaternion.LookRotation(rotateDir), rollRotationSpeed * Time.deltaTime);
+
+        if (_invulnerabilityTimer >= rollInvulnerabilityDuration)
+        {
+            _isInvulnerable = false;
+            _invulnerabilityTimer = 0.0f;
+        }
+    }
+    
+    void StartRoll()
+    {
+        // Set the player's velocity to the roll speed in the direction the player is currently facing
+        _endVel = rotateDir * rollSpeed;
+        
+        
+        // Play the roll animation
+        //_playerAnimator.SetTrigger("Roll");
+
+        // Set the invulnerability flag and reset the invulnerability timer
+        _isInvulnerable = true;
+        _invulnerabilityTimer = 0.0f;
+    }
+    
+    private Vector3 rotateDir;
+    private void RotatePlayer()
+    {
+        if (MoveFromCamera().sqrMagnitude >= 0.1f && !_isInvulnerable)
+        {
+            rotateDir = MoveFromCamera();
+            body.rotation = Quaternion.Lerp(body.rotation, Quaternion.LookRotation(rotateDir), rotationSpeed * Time.deltaTime);
+        }
     }
     
     private Vector3 Accelerate(Vector3 vel, float wishSpeed, float accel, Vector3 normal)
     {
-        Vector3 moveRight = Vector3.Cross(move, Vector3.up);
+        Vector3 moveRight = Vector3.Cross(MoveFromCamera(), Vector3.up);
         Vector3 planeMove = Vector3.Cross(normal, moveRight).normalized;
 
         float currentSpeed = Vector3.Dot(vel, planeMove);  //tf does this mean
@@ -81,10 +141,15 @@ public class PlayerMovement : MonoBehaviour
         Vector3 clampedSpeed = Vector3.ClampMagnitude(new Vector3(vel.x, 0, vel.z) + planeMove * accelSpeed, maxStrafeSpeed);
         return clampedSpeed + Vector3.up * vel.y;
     }
+
+    private Vector3 MoveFromCamera()
+    {
+        return (Quaternion.Euler(0, cam.transform.eulerAngles.y, 0) * move).normalized;
+    }
     
     private Vector3 Friction(Vector3 vel, float wishSpeed, float friction, Vector3 normal)
     {
-        Vector3 moveRight = Vector3.Cross(move, Vector3.up);
+        Vector3 moveRight = Vector3.Cross(MoveFromCamera(), Vector3.up);
         Vector3 planeMove = Vector3.Cross(normal, moveRight).normalized;
         Vector3 overDesired = vel - planeMove * wishSpeed;
 
