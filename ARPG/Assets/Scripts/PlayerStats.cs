543528217dfae3317b042bde14a4e65c3dc20931
M1crob1ology <lucabelluso123@gmail.com>
2022-12-13 14:21:40 +0100

    Perk to Perc
    perc 30 gang

--- ARPG/Assets/Scripts/PlayerStats.cs
+++ ARPG/Assets/Scripts/PlayerStats.cs
@@ -5,8 +5,8 @@ public class PlayerStats : MonoBehaviour, IDamageable
 {
     [Header("HP Variables")]
     public float baseMaxHealth = 100f;
-    public float maxHealthModPerk = 1f;
-    public float MaxHealth => baseMaxHealth * maxHealthModPerk;
+    public float maxHealthModPerc = 1f;
+    public float MaxHealth => baseMaxHealth * maxHealthModPerc;
     
     private float _currentHealth; //for clamp functionality
     public float CurrentHealth
@@ -14,8 +14,8 @@ public class PlayerStats : MonoBehaviour, IDamageable
 
     [Header("Mana Variables")]
     public float baseMaxMana = 100f;
-    public float maxManaModPerk = 1f;
-    public float MaxMana => baseMaxMana * maxManaModPerk;
+    public float maxManaModPerc = 1f;
+    public float MaxMana => baseMaxMana * maxManaModPerc;
     
     private float _currentMana; // for clamp functionality
     public float CurrentMana
@@ -23,8 +23,8 @@ public class PlayerStats : MonoBehaviour, IDamageable
     
     [Header("Player-Bound Attack Variables")]
     public float basePower = 10f;
-    public float powerModPerk = 1f;
-    public float AttackPower => basePower * powerModPerk;
+    public float powerModPerc = 1f;
+    public float AttackPower => basePower * powerModPerc;
 
     public float critRate = 0.05f; //inherent percentile - doesn't need ModPerc
     public float critDamage = 2f; //inherent percentile - doesn't need ModPerc
@@ -33,9 +33,9 @@ public class PlayerStats : MonoBehaviour, IDamageable
     [Header("Movement Speed Variables")]
     public float baseWalkMoveSpeed = 5f;
     public float baseRunMoveSpeed = 11f;
-    public float moveSpeedModPerk = 1f;
-    public float WalkMoveSpeed => baseWalkMoveSpeed * moveSpeedModPerk; 
-    public float RunMoveSpeed => baseRunMoveSpeed * moveSpeedModPerk; 
+    public float moveSpeedModPerc = 1f;
+    public float WalkMoveSpeed => baseWalkMoveSpeed * moveSpeedModPerc; 
+    public float RunMoveSpeed => baseRunMoveSpeed * moveSpeedModPerc; 
 
     
     
@@ -45,29 +45,29 @@ public class PlayerStats : MonoBehaviour, IDamageable
     public int maxDodgeCharges;
 
     public float baseDodgeSpeed = 20f;
-    public float dodgeSpeedModPerk = 1f;
-    public float DodgeSpeed => baseDodgeSpeed * dodgeSpeedModPerk;
+    public float dodgeSpeedModPerc = 1f;
+    public float DodgeSpeed => baseDodgeSpeed * dodgeSpeedModPerc;
     
     [Header("Passive Damage Mitigation Variables")]
-    public float damageTakenPercentage = 1; //inherent percentile - doesn't need ModPerk
-    public float evasionChance; //inherent percentile - doesn't need ModPerk
+    public float damageTakenPercentage = 1; //inherent percentile - doesn't need ModPerc
+    public float evasionChance; //inherent percentile - doesn't need ModPerc
     
     //'element' variables
     [Header("Element Variables")]
     [Header("Poison Variables")]
     public float basePoisonDamage = 1f;
-    public float poisonDamageModPerk = 1f;
+    public float poisonDamageModPerc = 1f;
     public float poisonLength = 5f;
-    public float PoisonDamage => basePoisonDamage * poisonDamageModPerk;
+    public float PoisonDamage => basePoisonDamage * poisonDamageModPerc;
     
     [Header("Fire Variables")]
     public float baseFireDamage = 1f;
-    public float fireDamageModPerk = 1f;
+    public float fireDamageModPerc = 1f;
     public float fireLength = 5f;
-    public float FireDamage => baseFireDamage * fireDamageModPerk;
+    public float FireDamage => baseFireDamage * fireDamageModPerc;
     
     [Header("Ice Variables")]
-    public float iceSlowdownPercentage = 0.2f; //inherent percentile - doesn't need ModPerk
+    public float iceSlowdownPercentage = 0.2f; //inherent percentile - doesn't need ModPerc
     public float iceLength = 5f;
     
     
