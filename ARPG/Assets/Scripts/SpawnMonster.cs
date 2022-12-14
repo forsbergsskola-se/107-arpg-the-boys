edee1fec9f4bd53b3f429e136fe75c35cb326ac7
KaleSmug <karl.sandegren@gmail.com>
2022-12-14 12:23:29 +0100

    Monster spawner spawns one monster

--- ARPG/Assets/Scripts/SpawnMonster.cs
+++ ARPG/Assets/Scripts/SpawnMonster.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,10 +6,21 @@ using UnityEngine;
 public class SpawnMonster : MonoBehaviour
 {
     public ParticleSystem spawnEffect;
-    
+    public LayerMask triggerSpawnLayers;
+    public bool readyToSpawn = true;
+    public GameObject enemy;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 7 && readyToSpawn)
+        {
+            readyToSpawn = false;
+            BeginSpawn();   
+        }
+    }
+
     void BeginSpawn()
     {
-            spawnEffect.Play();
-        
+        Instantiate(enemy, transform.position, transform.rotation);
+        spawnEffect.Play();
     }
 }
