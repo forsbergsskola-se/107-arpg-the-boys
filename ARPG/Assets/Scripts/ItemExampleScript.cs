f1ac1cbd2acffabd0cb96d6d38d1200ac0d72d1e
James Lind <112474995+Jlindhub@users.noreply.github.com>
2022-12-14 06:04:01 +0100

    nightly commit
    nightly commit of various stuff done - mostly testing.

---
+++ ARPG/Assets/Scripts/ItemExampleScript.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemExampleScript : MonoBehaviour
+{
+    
+    public ItemScriptableObject itemScriptableObject;
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        PlayerStats playerstats = collision.gameObject.GetComponent<PlayerStats>();
+        playerstats.baseMaxHealth += itemScriptableObject.testValue;
+    }
+
+}
