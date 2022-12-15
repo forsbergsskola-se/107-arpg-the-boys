fe34a26c599bef7b4a94c87eb09b9da489d66837
Bapmus <hampus.brink@elev.ga.lbs.se>
2022-12-15 11:57:10 +0100

    WhIIIILE

--- ARPG/Assets/Scripts/Enemy.cs
+++ ARPG/Assets/Scripts/Enemy.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using Unity.VisualScripting.Antlr3.Runtime.Collections;
 using UnityEditor;
 using UnityEngine;
@@ -9,7 +10,6 @@ using UnityEngine;
 public class LightAttack
 {
     public string lightAttackParameterNameOfTypeBool;
-    public GameObject lightAttackChild;
     public Vector3 lightAttackSize;
     public float lightAttackDamage;
     public float lightAttackStartDelay;
@@ -20,7 +20,8 @@ public class LightAttack
 public class HeavyAttack
 {
     public string heavyAttackParameterNameOfTypeBool;
-    public GameObject heavyAttackChild;
+    public Vector3 heavyAttackSize;
+    public float heavyAttackDamage;
     public float heavyAttackStartDelay;
     public float heavyAttackDurationDelay;
     public float heavyAttackDuration;
@@ -30,7 +31,7 @@ public class HeavyAttack
 public class Guard
 {
     public string guardParameterNameOfTypeBool;
-    public GameObject guardChild;
+    public Vector3 guardSize;
     public float guardStartDelay;
     public float guardDurationDelay;
     public float guardDuration;
@@ -62,7 +63,6 @@ public class Enemy : MonoBehaviour, Iinteruptable
     private bool _isAttacking;
     private Coroutine _startedAttack;
 
-    private GameObject _currentAttackChild;
     private string _currentAttackParameter;
     public Transform attackTransform;
     public LayerMask hitLayer;
@@ -77,7 +77,7 @@ public class Enemy : MonoBehaviour, Iinteruptable
     // Update is called once per frame
     void Update()
     {
-        bool inDistance = Vector3.Distance(transform.position, target.transform.position) < attackRange;
+        var inDistance = Vector3.Distance(transform.position, target.transform.position) < attackRange;
 
         if (inDistance && !_isAttacking)
         {
@@ -136,20 +136,24 @@ public class Enemy : MonoBehaviour, Iinteruptable
     private IEnumerator CO_EnemyLightAttack()
     {
         _isAttacking = true;
-        _currentAttackChild = lightAttackInformation.lightAttackChild;
+        float timer = 0;
         _currentAttackParameter = lightAttackInformation.lightAttackParameterNameOfTypeBool;
         yield return new WaitForSeconds(lightAttackInformation.lightAttackStartDelay);
         animator.SetBool(lightAttackInformation.lightAttackParameterNameOfTypeBool, true);
         yield return new WaitForSeconds(lightAttackInformation.lightAttackDurationDelay);
-        lightAttackInformation.lightAttackChild.SetActive(true);
-        Collider[] hits = Physics.OverlapBox(attackTransform.position,lightAttackInformation.lightAttackSize / 2, Quaternion.identity, hitLayer);
-        for (var i = 0; i < hits.Length; i++)
+        while (timer < lightAttackInformation.lightAttackDuration)
         {
-            if (hits[i].TryGetComponent(out IDamageable damageable))
-                damageable.TakeDamage(lightAttackInformation.lightAttackDamage);
+            Collider[] hits = Physics.OverlapBox(attackTransform.position,lightAttackInformation.lightAttackSize / 2, Quaternion.identity, hitLayer);
+            for (var i = 0; i < hits.Length; i++)
+            {
+                if (hits[i].TryGetComponent(out IDamageable damageable))
+                    damageable.TakeDamage(lightAttackInformation.lightAttackDamage);
+            }
+
+            timer += Time.deltaTime;
+            yield return null;
         }
         yield return new WaitForSeconds(lightAttackInformation.lightAttackDuration);
-        lightAttackInformation.lightAttackChild.SetActive(false);
         animator.SetBool(lightAttackInformation.lightAttackParameterNameOfTypeBool, false);
         _isAttacking = false;
     }
@@ -157,14 +161,23 @@ public class Enemy : MonoBehaviour, Iinteruptable
     private IEnumerator CO_EnemyHeavyAttack()
     {
         _isAttacking = true;
-        _currentAttackChild = heavyAttackInformation.heavyAttackChild;
+        float timer = 0;
         _currentAttackParameter = heavyAttackInformation.heavyAttackParameterNameOfTypeBool;
         yield return new WaitForSeconds(heavyAttackInformation.heavyAttackStartDelay);
         animator.SetBool(heavyAttackInformation.heavyAttackParameterNameOfTypeBool, true);
         yield return new WaitForSeconds(heavyAttackInformation.heavyAttackDurationDelay);
-        heavyAttackInformation.heavyAttackChild.SetActive(true);
-        yield return new WaitForSeconds(heavyAttackInformation.heavyAttackDuration);
-        heavyAttackInformation.heavyAttackChild.SetActive(false);
+        while (timer < heavyAttackInformation.heavyAttackDuration)
+        {
+            Collider[] hits = Physics.OverlapBox(attackTransform.position,heavyAttackInformation.heavyAttackSize / 2, Quaternion.identity, hitLayer);
+            for (var i = 0; i < hits.Length; i++)
+            {
+                if (hits[i].TryGetComponent(out IDamageable damageable))
+                    damageable.TakeDamage(heavyAttackInformation.heavyAttackDamage);
+            }
+            timer += Time.deltaTime;
+            yield return null;
+        }
+        
         animator.SetBool(heavyAttackInformation.heavyAttackParameterNameOfTypeBool, false);
         _isAttacking = false;
     }
@@ -172,14 +185,24 @@ public class Enemy : MonoBehaviour, Iinteruptable
     private IEnumerator CO_EnemyGuard()
     {
         _isAttacking = true;
-        _currentAttackChild = guardInformation.guardChild;
+        float timer = 0;
         _currentAttackParameter = guardInformation.guardParameterNameOfTypeBool;
         yield return new WaitForSeconds(guardInformation.guardStartDelay);
         animator.SetBool(guardInformation.guardParameterNameOfTypeBool, true);
         yield return new WaitForSeconds(guardInformation.guardDurationDelay);
-        guardInformation.guardChild.SetActive(true);
-        yield return new WaitForSeconds(guardInformation.guardDuration);
-        guardInformation.guardChild.SetActive(false);
+        while (timer < guardInformation.guardDuration)
+        {
+            Collider[] hits = Physics.OverlapBox(attackTransform.position,guardInformation.guardSize / 2, Quaternion.identity, hitLayer);
+            for (var i = 0; i < hits.Length; i++)
+            {
+                if (hits[i].TryGetComponent(out IDamageable damageable))
+                    damageable.TakeDamage(heavyAttackInformation.heavyAttackDamage);
+                //change for guard
+            }
+            DrawBoxCastBox(attackTransform.position,lightAttackInformation.lightAttackSize / 2, Quaternion.identity, Color.magenta);
+            timer += Time.deltaTime;
+            yield return null;
+        }
         animator.SetBool(guardInformation.guardParameterNameOfTypeBool, false);
         _isAttacking = false;
     }
@@ -194,7 +217,103 @@ public class Enemy : MonoBehaviour, Iinteruptable
     {
         StopCoroutine(_startedAttack);
         _isAttacking = false;
-        _currentAttackChild.SetActive(false);
         animator.SetBool(_currentAttackParameter, false);
     }
+    // TA BORT ALL DET HÄR SEN!!! SNÄLLA SNÄLLA SNÄLLA
+    //Draws just the box at where it is currently hitting.
+    public static void DrawBoxCastOnHit(Vector3 origin, Vector3 halfExtents, Quaternion orientation, Vector3 direction, float hitInfoDistance, Color color)
+    {
+        origin = CastCenterOnCollision(origin, direction, hitInfoDistance);
+        DrawBox(origin, halfExtents, orientation, color);
+    }
+    
+    
+     public static void DrawBoxCastBox(Vector3 origin, Vector3 halfExtents, Quaternion orientation, Color color)
+     {
+         Box bottomBox = new Box(origin, halfExtents, orientation);
+         DrawBox(bottomBox, color);
+     }
+     
+     public static void DrawBox(Vector3 origin, Vector3 halfExtents, Quaternion orientation, Color color)
+     {
+         DrawBox(new Box(origin, halfExtents, orientation), color);
+     }
+     public static void DrawBox(Box box, Color color)
+     {
+         Debug.DrawLine(box.frontTopLeft,     box.frontTopRight,    color);
+         Debug.DrawLine(box.frontTopRight,     box.frontBottomRight, color);
+         Debug.DrawLine(box.frontBottomRight, box.frontBottomLeft, color);
+         Debug.DrawLine(box.frontBottomLeft,     box.frontTopLeft, color);
+                                                  
+         Debug.DrawLine(box.backTopLeft,         box.backTopRight, color);
+         Debug.DrawLine(box.backTopRight,     box.backBottomRight, color);
+         Debug.DrawLine(box.backBottomRight,     box.backBottomLeft, color);
+         Debug.DrawLine(box.backBottomLeft,     box.backTopLeft, color);
+                                                  
+         Debug.DrawLine(box.frontTopLeft,     box.backTopLeft, color);
+         Debug.DrawLine(box.frontTopRight,     box.backTopRight, color);
+         Debug.DrawLine(box.frontBottomRight, box.backBottomRight, color);
+         Debug.DrawLine(box.frontBottomLeft,     box.backBottomLeft, color);
+     }
+     
+     
+     
+     
+     public struct Box
+     {
+         public Vector3 localFrontTopLeft     {get; private set;}
+         public Vector3 localFrontTopRight    {get; private set;}
+         public Vector3 localFrontBottomLeft  {get; private set;}
+         public Vector3 localFrontBottomRight {get; private set;}
+         public Vector3 localBackTopLeft      {get {return -localFrontBottomRight;}}
+         public Vector3 localBackTopRight     {get {return -localFrontBottomLeft;}}
+         public Vector3 localBackBottomLeft   {get {return -localFrontTopRight;}}
+         public Vector3 localBackBottomRight  {get {return -localFrontTopLeft;}}
+ 
+         public Vector3 frontTopLeft     {get {return localFrontTopLeft + origin;}}
+         public Vector3 frontTopRight    {get {return localFrontTopRight + origin;}}
+         public Vector3 frontBottomLeft  {get {return localFrontBottomLeft + origin;}}
+         public Vector3 frontBottomRight {get {return localFrontBottomRight + origin;}}
+         public Vector3 backTopLeft      {get {return localBackTopLeft + origin;}}
+         public Vector3 backTopRight     {get {return localBackTopRight + origin;}}
+         public Vector3 backBottomLeft   {get {return localBackBottomLeft + origin;}}
+         public Vector3 backBottomRight  {get {return localBackBottomRight + origin;}}
+ 
+         public Vector3 origin {get; private set;}
+ 
+         public Box(Vector3 origin, Vector3 halfExtents, Quaternion orientation) : this(origin, halfExtents)
+         {
+             Rotate(orientation);
+         }
+         public Box(Vector3 origin, Vector3 halfExtents)
+         {
+             this.localFrontTopLeft     = new Vector3(-halfExtents.x, halfExtents.y, -halfExtents.z);
+             this.localFrontTopRight    = new Vector3(halfExtents.x, halfExtents.y, -halfExtents.z);
+             this.localFrontBottomLeft  = new Vector3(-halfExtents.x, -halfExtents.y, -halfExtents.z);
+             this.localFrontBottomRight = new Vector3(halfExtents.x, -halfExtents.y, -halfExtents.z);
+ 
+             this.origin = origin;
+         }
+ 
+ 
+         public void Rotate(Quaternion orientation)
+         {
+             localFrontTopLeft     = RotatePointAroundPivot(localFrontTopLeft    , Vector3.zero, orientation);
+             localFrontTopRight    = RotatePointAroundPivot(localFrontTopRight   , Vector3.zero, orientation);
+             localFrontBottomLeft  = RotatePointAroundPivot(localFrontBottomLeft , Vector3.zero, orientation);
+             localFrontBottomRight = RotatePointAroundPivot(localFrontBottomRight, Vector3.zero, orientation);
+         }
+     }
+ 
+      //This should work for all cast types
+     static Vector3 CastCenterOnCollision(Vector3 origin, Vector3 direction, float hitInfoDistance)
+     {
+         return origin + (direction.normalized * hitInfoDistance);
+     }
+     
+     static Vector3 RotatePointAroundPivot(Vector3 point, Vector3 pivot, Quaternion rotation)
+     {
+         Vector3 direction = point - pivot;
+         return pivot + rotation * direction;
+     }
 }
