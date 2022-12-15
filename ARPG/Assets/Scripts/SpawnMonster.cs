444defe723934785e1ae17801d33c94b532b5399
KaleSmug <karl.sandegren@gmail.com>
2022-12-15 13:18:25 +0100

    Monsters spawnrate can be adjusted

--- ARPG/Assets/Scripts/SpawnMonster.cs
+++ ARPG/Assets/Scripts/SpawnMonster.cs
@@ -1,14 +1,18 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Serialization;
+using Random = UnityEngine.Random;
 
 public class SpawnMonster : MonoBehaviour
 {
     public ParticleSystem spawnEffect;
     public LayerMask triggerSpawnLayers;
     public bool readyToSpawn = true;
-    public GameObject enemy;
+    public GameObject[] enemies;
+    [FormerlySerializedAs("enemiesSpawnRate")] public int[] enemiesSpawnRates;
     private void OnTriggerEnter(Collider other)
     {
         if ((triggerSpawnLayers & (1 << other.gameObject.layer)) != 0 && readyToSpawn)
@@ -20,7 +24,32 @@ public class SpawnMonster : MonoBehaviour
 
     void BeginSpawn()
     {
-        Instantiate(enemy, transform.position, transform.rotation);
+        // all enemies have a number of lapps they put into a skål. this counts all the lapps.
+        int fullRateAmount = 0;
+        for (int i = 0; i < enemiesSpawnRates.Length; i++)
+        {
+            for (int j = 0; j < enemiesSpawnRates[i]; j++)
+            {
+                fullRateAmount++;
+            }
+        }
+        // picks a random lapp
+        int thisEnemy = Random.Range(0, fullRateAmount);
+        
+        //checks what lapp represents which enemy
+        for (int i = 0; i < enemiesSpawnRates.Length; i++)
+        {
+            for (int j = 0; j < enemiesSpawnRates[i]; j++)
+            {
+                if (j == thisEnemy)
+                {
+                    thisEnemy = i;
+                    break;
+                }
+            }
+        }
+        
+        Instantiate(enemies[thisEnemy], transform.position, transform.rotation);
         spawnEffect.Play();
     }
 }
