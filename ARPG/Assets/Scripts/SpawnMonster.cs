e85427a6bf05b3bf443d974b6ed3ddd71335807a
KaleSmug <karl.sandegren@gmail.com>
2022-12-15 13:52:35 +0100

    Spawnrate bugfix

--- ARPG/Assets/Scripts/SpawnMonster.cs
+++ ARPG/Assets/Scripts/SpawnMonster.cs
@@ -36,19 +36,23 @@ public class SpawnMonster : MonoBehaviour
         // picks a random lapp
         int thisEnemy = Random.Range(0, fullRateAmount);
         
+        Debug.Log(thisEnemy +" is the start lapp");
         //checks what lapp represents which enemy
+        int _countedTimes = 0;
         for (int i = 0; i < enemiesSpawnRates.Length; i++)
         {
             for (int j = 0; j < enemiesSpawnRates[i]; j++)
             {
-                if (j == thisEnemy)
+                if (thisEnemy == _countedTimes)
                 {
                     thisEnemy = i;
+                    Debug.Log(i +" is the enemy we found the number at");
                     break;
                 }
+                _countedTimes++;
             }
         }
-        
+        Debug.Log(thisEnemy +" is the chosen enemy from the array");
         Instantiate(enemies[thisEnemy], transform.position, transform.rotation);
         spawnEffect.Play();
     }
