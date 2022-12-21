77db8948dd278c5398304736946b61fc767de439
M1crob1ology <lucabelluso123@gmail.com>
2022-12-21 12:45:17 +0100

    Added roll/dash cooldown

--- ARPG/Assets/Scripts/PlayerCombat.cs
+++ ARPG/Assets/Scripts/PlayerCombat.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 using Quaternion = UnityEngine.Quaternion;
 using Random = UnityEngine.Random;
 using Vector3 = UnityEngine.Vector3;
+using static DrawBoxCast;
 
 public class PlayerCombat : MonoBehaviour, IInterruptible
 {
@@ -44,15 +45,15 @@ public class PlayerCombat : MonoBehaviour, IInterruptible
         {
             if (Input.GetButtonDown("Fire1"))
             {
-                _currentAttack = StartCoroutine(Attack(currentWeapon.lightAttackSpeed, "Light-attack", IInterruptible.AttackState.LightAttack));
+                _currentAttack = StartCoroutine(CO_Attack(currentWeapon.lightAttackSpeed, "Light-attack", IInterruptible.AttackState.LightAttack));
             }
             if (Input.GetButtonDown("Fire2"))
             {
-                _currentAttack = StartCoroutine(Attack(currentWeapon.heavyAttackSpeed, "Heavy-attack", IInterruptible.AttackState.HeavyAttack));
+                _currentAttack = StartCoroutine(CO_Attack(currentWeapon.heavyAttackSpeed, "Heavy-attack", IInterruptible.AttackState.HeavyAttack));
             }
             if (Input.GetButtonDown("Fire3"))
             {
-                _currentAttack = StartCoroutine(Guard(currentWeapon.guardTime, currentWeapon.parryTime));
+                _currentAttack = StartCoroutine(CO_Guard(currentWeapon.guardTime, currentWeapon.parryTime));
             }
         }
 
@@ -98,7 +99,7 @@ public class PlayerCombat : MonoBehaviour, IInterruptible
         }
     }
 
-    private IEnumerator Attack(float attackSpeed, string animTriggerName, IInterruptible.AttackState attackState)
+    private IEnumerator CO_Attack(float attackSpeed, string animTriggerName, IInterruptible.AttackState attackState)
     {
         CurrentAttackState = attackState;
         Debug.Log(animTriggerName);
@@ -124,11 +125,13 @@ public class PlayerCombat : MonoBehaviour, IInterruptible
         CurrentAttackState = IInterruptible.AttackState.NoAttack;
     }
 
-    private IEnumerator Guard(float guardTime, float parryTime)
+    private IEnumerator CO_Guard(float guardTime, float parryTime)
     {
         Debug.Log("Guarded!");
 
-        _playerMovement.playerAnimator.speed = guardTime;
+        
+        
+        _playerMovement.playerAnimator.speed = 1f / guardTime;
         animationEnded = false;
         isAttacking = true;
         _playerMovement._rb.velocity = Vector3.zero;
@@ -251,104 +254,4 @@ public class PlayerCombat : MonoBehaviour, IInterruptible
     }
 
     public IInterruptible.AttackState CurrentAttackState { get; set; }
-    
-
-    // TA BORT ALL DET HÄR SEN!!! SNÄLLA SNÄLLA SNÄLLA
-    //Draws just the box at where it is currently hitting.
-    public static void DrawBoxCastOnHit(Vector3 origin, Vector3 halfExtents, Quaternion orientation, Vector3 direction, float hitInfoDistance, Color color)
-    {
-        origin = CastCenterOnCollision(origin, direction, hitInfoDistance);
-        DrawBox(origin, halfExtents, orientation, color);
-    }
-    
-    
-     public static void DrawBoxCastBox(Vector3 origin, Vector3 halfExtents, Quaternion orientation, Color color)
-     {
-         Box bottomBox = new Box(origin, halfExtents, orientation);
-         DrawBox(bottomBox, color);
-     }
-     
-     public static void DrawBox(Vector3 origin, Vector3 halfExtents, Quaternion orientation, Color color)
-     {
-         DrawBox(new Box(origin, halfExtents, orientation), color);
-     }
-     public static void DrawBox(Box box, Color color)
-     {
-         Debug.DrawLine(box.frontTopLeft,     box.frontTopRight,    color);
-         Debug.DrawLine(box.frontTopRight,     box.frontBottomRight, color);
-         Debug.DrawLine(box.frontBottomRight, box.frontBottomLeft, color);
-         Debug.DrawLine(box.frontBottomLeft,     box.frontTopLeft, color);
-                                                  
-         Debug.DrawLine(box.backTopLeft,         box.backTopRight, color);
-         Debug.DrawLine(box.backTopRight,     box.backBottomRight, color);
-         Debug.DrawLine(box.backBottomRight,     box.backBottomLeft, color);
-         Debug.DrawLine(box.backBottomLeft,     box.backTopLeft, color);
-                                                  
-         Debug.DrawLine(box.frontTopLeft,     box.backTopLeft, color);
-         Debug.DrawLine(box.frontTopRight,     box.backTopRight, color);
-         Debug.DrawLine(box.frontBottomRight, box.backBottomRight, color);
-         Debug.DrawLine(box.frontBottomLeft,     box.backBottomLeft, color);
-     }
-     
-     
-     
-     
-     public struct Box
-     {
-         public Vector3 localFrontTopLeft     {get; private set;}
-         public Vector3 localFrontTopRight    {get; private set;}
-         public Vector3 localFrontBottomLeft  {get; private set;}
-         public Vector3 localFrontBottomRight {get; private set;}
-         public Vector3 localBackTopLeft      {get {return -localFrontBottomRight;}}
-         public Vector3 localBackTopRight     {get {return -localFrontBottomLeft;}}
-         public Vector3 localBackBottomLeft   {get {return -localFrontTopRight;}}
-         public Vector3 localBackBottomRight  {get {return -localFrontTopLeft;}}
- 
-         public Vector3 frontTopLeft     {get {return localFrontTopLeft + origin;}}
-         public Vector3 frontTopRight    {get {return localFrontTopRight + origin;}}
-         public Vector3 frontBottomLeft  {get {return localFrontBottomLeft + origin;}}
-         public Vector3 frontBottomRight {get {return localFrontBottomRight + origin;}}
-         public Vector3 backTopLeft      {get {return localBackTopLeft + origin;}}
-         public Vector3 backTopRight     {get {return localBackTopRight + origin;}}
-         public Vector3 backBottomLeft   {get {return localBackBottomLeft + origin;}}
-         public Vector3 backBottomRight  {get {return localBackBottomRight + origin;}}
- 
-         public Vector3 origin {get; private set;}
- 
-         public Box(Vector3 origin, Vector3 halfExtents, Quaternion orientation) : this(origin, halfExtents)
-         {
-             Rotate(orientation);
-         }
-         public Box(Vector3 origin, Vector3 halfExtents)
-         {
-             this.localFrontTopLeft     = new Vector3(-halfExtents.x, halfExtents.y, -halfExtents.z);
-             this.localFrontTopRight    = new Vector3(halfExtents.x, halfExtents.y, -halfExtents.z);
-             this.localFrontBottomLeft  = new Vector3(-halfExtents.x, -halfExtents.y, -halfExtents.z);
-             this.localFrontBottomRight = new Vector3(halfExtents.x, -halfExtents.y, -halfExtents.z);
- 
-             this.origin = origin;
-         }
- 
- 
-         public void Rotate(Quaternion orientation)
-         {
-             localFrontTopLeft     = RotatePointAroundPivot(localFrontTopLeft    , Vector3.zero, orientation);
-             localFrontTopRight    = RotatePointAroundPivot(localFrontTopRight   , Vector3.zero, orientation);
-             localFrontBottomLeft  = RotatePointAroundPivot(localFrontBottomLeft , Vector3.zero, orientation);
-             localFrontBottomRight = RotatePointAroundPivot(localFrontBottomRight, Vector3.zero, orientation);
-         }
-     }
- 
-      //This should work for all cast types
-     static Vector3 CastCenterOnCollision(Vector3 origin, Vector3 direction, float hitInfoDistance)
-     {
-         return origin + (direction.normalized * hitInfoDistance);
-     }
-     
-     static Vector3 RotatePointAroundPivot(Vector3 point, Vector3 pivot, Quaternion rotation)
-     {
-         Vector3 direction = point - pivot;
-         return pivot + rotation * direction;
-     }
-     
 }
