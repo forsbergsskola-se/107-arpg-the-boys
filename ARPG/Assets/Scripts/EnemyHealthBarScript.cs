9bbce72e87041c8722b1067e4319f3478ad62199
Leo Sever <leo.e.sever@gmail.com>
2022-12-21 15:30:27 +0100

    started working on enemy healthbar

---
+++ ARPG/Assets/Scripts/EnemyHealthBarScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHealthBarScript : MonoBehaviour
+{
+    public Slider slider;
+    public Enemy enemy;
+
+    public void Start()
+    {
+        SetMaxhealth(enemy.maxHealth);
+    }
+
+    public void FixedUpdate()
+    {
+        SetHealth(enemy.CurrentHealth);
+    }
+
+    public void SetMaxhealth(float health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+    }
+
+    public void SetHealth(float health)
+    {
+        slider.value = health;
+    }
+}
