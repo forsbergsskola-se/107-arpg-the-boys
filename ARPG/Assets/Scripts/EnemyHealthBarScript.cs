6d555716a184d5261a8f344a59009138d322d335
Leo S <leo.e.sever@gmail.com>
2022-12-22 10:09:54 +0100

    Fixed so enemy healthbar always faces the Main Camera

--- ARPG/Assets/Scripts/EnemyHealthBarScript.cs
+++ ARPG/Assets/Scripts/EnemyHealthBarScript.cs
@@ -7,15 +7,18 @@ public class EnemyHealthBarScript : MonoBehaviour
 {
     public Slider slider;
     public Enemy enemy;
+    private Camera _camera;
 
     public void Start()
     {
         SetMaxhealth(enemy.maxHealth);
+        _camera = Camera.main;
     }
 
     public void FixedUpdate()
     {
         SetHealth(enemy.CurrentHealth);
+        transform.rotation = Quaternion.LookRotation(transform.position - _camera.transform.position);
     }
 
     public void SetMaxhealth(float health)
