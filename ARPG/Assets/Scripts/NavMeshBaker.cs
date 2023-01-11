ff8e393668e572a73318f1b4937b6f13ce54aac0
Leo Sever <leo.e.sever@gmail.com>
2023-01-11 14:05:05 +0100

    small changes

--- ARPG/Assets/Scripts/NavMeshBaker.cs
+++ ARPG/Assets/Scripts/NavMeshBaker.cs
@@ -6,15 +6,18 @@ using UnityEngine;
 public class NavMeshBaker : MonoBehaviour
 {
     private NavMeshSurface[] navMeshSurfaces;
+    private NavMeshSurface[] _navMeshSurfaces;
+
     void Start()
     {
+        _navMeshSurfaces = FindObjectsOfType<NavMeshSurface>();
         StartCoroutine(WaitForSpawn());
     }
 
     public IEnumerator WaitForSpawn()
     {
         yield return new WaitForSeconds(1);
-        navMeshSurfaces = FindObjectsOfType<NavMeshSurface>();
+        navMeshSurfaces = _navMeshSurfaces;
         for (var i = 0; i < navMeshSurfaces.Length; i++)
         {
             navMeshSurfaces[i].BuildNavMesh();
